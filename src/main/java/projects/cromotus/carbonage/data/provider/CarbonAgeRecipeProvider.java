7922cde9059aaf7f2f484ef8db078926db4a152f
Cromotus <linbgm@gmail.com>
2020-03-19 11:49:18 +0100

    Shifted recipes to generated ones

--- src/main/java/projects/cromotus/carbonage/data/provider/CarbonAgeRecipeProvider.java
+++ src/main/java/projects/cromotus/carbonage/data/provider/CarbonAgeRecipeProvider.java
@@ -30,9 +30,14 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.Tag;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.Tags;
 import projects.cromotus.carbonage.CarbonAge;
+import projects.cromotus.carbonage.data.tags.CarbonAgeTags;
+import projects.cromotus.carbonage.init.CarbonAgeBlocks;
 import projects.cromotus.carbonage.init.CarbonAgeItems;
 
 import java.util.Arrays;
@@ -47,6 +52,7 @@ public class CarbonAgeRecipeProvider extends RecipeProvider {
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        //Shaped Recipes
         ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.MORTAR_STONE, 1)
                 .patternLine("S")
                 .patternLine("S")
@@ -56,11 +62,113 @@ public class CarbonAgeRecipeProvider extends RecipeProvider {
                 .addCriterion("has_stone", hasItem(Blocks.STONE))
                 .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mortar_stone"));
 
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.MORTAR_IRON, 1)
+                .patternLine("I")
+                .patternLine("I")
+                .patternLine("B")
+                .key('I', Items.IRON_INGOT)
+                .key('B', CarbonAgeItems.STONE_BOWL)
+                .addCriterion("has_iron", hasItem(Items.IRON_INGOT))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mortar_iron"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.MORTAR_DIAMOND, 1)
+                .patternLine("D")
+                .patternLine("D")
+                .patternLine("B")
+                .key('D', Items.DIAMOND)
+                .key('B', CarbonAgeItems.STONE_BOWL)
+                .addCriterion("has_diamond", hasItem(Items.DIAMOND))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mortar_diamond"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.STONE_BOWL, 1)
+                .patternLine("S S")
+                .patternLine("SSS")
+                .key('S', Blocks.STONE)
+                .addCriterion("has_stone", hasItem(Blocks.STONE))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "stone_bowl"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.CARBON_MASS, 1)
+                .patternLine(" # ")
+                .patternLine("#W#")
+                .patternLine(" # ")
+                .key('#', CarbonAgeItems.DENSE_COAL_DUST)
+                .key('W', Items.WATER_BUCKET)
+                .addCriterion("has_dense_coal_dust", hasItem(CarbonAgeItems.DENSE_COAL_DUST))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "carbon_mass"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.IRON_STICK, 4)
+                .patternLine("I")
+                .patternLine("I")
+                .key('I', Items.IRON_INGOT)
+                .addCriterion("has_iron_ingot", hasItem(Items.IRON_INGOT))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "iron_stick"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.MOLD_EMPTY, 1)
+                .patternLine("III")
+                .patternLine("S S")
+                .patternLine("III")
+                .key('I', Items.IRON_INGOT)
+                .key('S', CarbonAgeItems.IRON_STICK)
+                .addCriterion("has_iron_stick", hasItem(CarbonAgeItems.IRON_STICK))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mold_empty"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeItems.MOLD_FILLED, 1)
+                .patternLine(" C ")
+                .patternLine("CMC")
+                .patternLine(" C ")
+                .key('C', CarbonAgeItems.CARBON_MASS)
+                .key('M', CarbonAgeItems.MOLD_EMPTY)
+                .addCriterion("has_mold", hasItem(CarbonAgeItems.MOLD_EMPTY))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mold_filled"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeBlocks.CARBON_BLOCK, 4)
+                .patternLine(" C ")
+                .patternLine("CSC")
+                .patternLine(" C ")
+                .key('C', CarbonAgeItems.CARBON_PLATE)
+                .key('S', Blocks.STONE)
+                .addCriterion("has_carbon", hasItem(CarbonAgeItems.CARBON_PLATE))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "carbon_block"));
+
+        ShapedRecipeBuilder.shapedRecipe(CarbonAgeBlocks.DENSE_CARBON_BLOCK, 4)
+                .patternLine(" C ")
+                .patternLine("CSC")
+                .patternLine(" C ")
+                .key('C', CarbonAgeItems.DENSE_CARBON_PLATE)
+                .key('S', Blocks.STONE)
+                .addCriterion("has_dense_carbon", hasItem(CarbonAgeItems.DENSE_CARBON_PLATE))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "dense_carbon_block"));
+
+
+        //Shapeless Recipes
+        ShapelessRecipeBuilder.shapelessRecipe(CarbonAgeItems.TINY_COAL_DUST, 1)
+                .addIngredient(Items.COAL, 1)
+                .addIngredient(CarbonAgeTags.Items.MORTAR)
+                .addCriterion("has_mortar", hasItem(CarbonAgeTags.Items.MORTAR))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "tiny_coal_dust"));
+
         ShapelessRecipeBuilder.shapelessRecipe(CarbonAgeItems.COAL_DUST, 1)
                 .addIngredient(CarbonAgeItems.TINY_COAL_DUST, 4)
                 .addCriterion("has_tiny_coal_dust", hasItem(CarbonAgeItems.TINY_COAL_DUST))
                 .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "coal_dust"));
 
+        ShapelessRecipeBuilder.shapelessRecipe(CarbonAgeItems.DENSE_COAL_DUST, 1)
+                .addIngredient(CarbonAgeItems.COAL_DUST, 4)
+                .addCriterion("has_coal_dust", hasItem(CarbonAgeItems.COAL_DUST))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "dense_coal_dust"));
+
+        ShapelessRecipeBuilder.shapelessRecipe(CarbonAgeItems.CARBON_PLATE, 1)
+                .addIngredient(CarbonAgeItems.MOLD_DRIED)
+                .addCriterion("has_mold_dried", hasItem(CarbonAgeItems.MOLD_DRIED))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "carbon_plate"));
+
+        ShapelessRecipeBuilder.shapelessRecipe(CarbonAgeItems.DENSE_CARBON_PLATE, 1)
+                .addIngredient(CarbonAgeItems.CARBON_PLATE, 9)
+                .addCriterion("has_carbon_plate", hasItem(CarbonAgeItems.CARBON_PLATE))
+                .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "dense_carbon_plate"));
+
+
+        //Smelting
         CookingRecipeBuilder.smeltingRecipe(Ingredient.fromItems(CarbonAgeItems.MOLD_FILLED), CarbonAgeItems.MOLD_DRIED, 0.1f, 200)
                 .addCriterion("has_mold_filled", hasItem(CarbonAgeItems.MOLD_FILLED))
                 .build(consumer, new ResourceLocation(CarbonAge.MOD_ID, "mold_dried"));
